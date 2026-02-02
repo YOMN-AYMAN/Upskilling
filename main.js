de74e0fed999f5033ead0c11da43874b2190ceaf
yomna677 <yomnaaym@gmail.com>
2026-02-02 23:57:41 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,98 @@
+// ========== task session 3 ================
+// var number = document.getElementById("number");
+// function calcSum(){
+//     var sum = 0 , evenCount = 0 , oddCount = 0;
+//     for(var i=1 ; i<=number.value ; i++){
+//         sum+=i;
+//         if(i%2===0){
+//             evenCount+=1
+//         }
+//         else{
+//             oddCount+=1
+//         }
+//         document.getElementById("numbers").innerHTML += `${i}  `;
+//     };
+//     document.getElementById("demo").innerHTML = `<span>the sum is : ${sum} <br> number of even numbers : ${evenCount} <br> number of odd numbers : ${oddCount}</span>`;
+// };
+
+
+// ============= task session 2 ================
+// Q1
+// var number = Number(window.prompt("enter a number : "));
+// console.log("the number is : " + number);
+
+
+// Q2
+// var number = Number(window.prompt("enter a number : "));
+// if(number % 3 === 0 && number % 4 === 0){
+//     console.log("yes");
+// }
+// else{
+//     console.log("no");
+// }
+
+
+// Q3
+// var num1 = Number(window.prompt("enter a num1 : "));
+// var num2 = Number(window.prompt("enter a num2 : "));
+// if(num1>num2){
+//     console.log('max is : ' + num1);
+// }
+// else{
+//     console.log('max is : ' + num2);
+// }
+
+
+// Q4
+// var number = Number(window.prompt("enter a number : "));
+// if(number<0){
+//     console.log("negative");
+// }
+// else{
+//     console.log("positive");
+// }
+
+
+
+// Q5
+// var num1 = Number(window.prompt("enter a num1 : "));
+// var num2 = Number(window.prompt("enter a num2 : "));
+// var num3 = Number(window.prompt("enter a num3 : "));
+// var maximum , minimum ;
+// if(num1>=num2 && num1>=num3){
+//     maximum = num1
+// }
+// else if(num2>=num1 && num2>=num3){
+//     maximum = num2
+// }
+// else{
+//     maximum = num3
+// }
+// if(num1<=num2 && num1<=num3){
+//     minimum = num1
+// }
+// else if(num2<=num1 && num2<=num3){
+//     minimum = num2
+// }
+// else{
+//     minimum = num3
+// }
+// console.log("the maximum element is : " + maximum)
+// console.log("the minimum element is : " + minimum)
+
+
+
+
+
+//  ========== task session 1 =========== 
+// var userName = window.prompt("enter your name : ");
+// var userAge = window.prompt("enter your age : ");
+
+// var nameQ = document.getElementById("name");
+// var ageQ = document.getElementById("age");
+
+// var theName = document.getElementById("name").innerHTML;
+// var theAge = document.getElementById("age").innerHTML;
+
+// nameQ.innerHTML = theName + userName
+// ageQ.innerHTML = theAge + userAge
