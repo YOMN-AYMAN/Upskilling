a4b452f173bbea5c111076e6db2ec78e8ee04552
yomna677 <yomnaaym@gmail.com>
2026-03-01 16:47:22 +0200

    task of session 9

--- main.js
+++ main.js
@@ -1,138 +1,181 @@
 
+// ========== session 9 ============
+
+// ===> Q1
+let fs = require('fs')
+
+fs.writeFileSync('notes.txt', 'hello, node.js')
+console.log('file created.')
+
+fs.appendFileSync('notes.txt', ' learning fs module')
+console.log('text appended.')
+
+let data = fs.readFileSync('notes.txt')
+console.log('file content : ' + data)
+
+fs.unlinkSync('notes.txt')
+console.log('file deleted.')
+
+
+
+// ===>Q2
+let os = require('os')
+console.log('architecture: ' + os.arch())
+console.log('platform: ' + os.platform())
+console.log('total memory: ' + (os.totalmem()))
+console.log('hostname: ' + os.hostname())
+console.log('uptime: ' + os.uptime())
+
+
+
+// ===>Q3
+let http = require('http')
+
+let server = http.createServer((req, res) => {
+    res.end('i did it')
+})
+
+server.listen(3000, () => {
+    console.log('server is running')
+})
+
+
+
+
 
 // ========== session 8 ============
-// ===> Q1
-for (let i = 1; i <= 5; i++) {
-    console.log(i);
-}
-console.log("done");
+// // ===> Q1
+// for (let i = 1; i <= 5; i++) {
+//     console.log(i);
+// }
+// console.log("done");
 
 
-// ===> Q2
-// console.log("start");
+// // ===> Q2
+// // console.log("start");
 
-setTimeout(() => {
-    console.log("hello A-sync");
-}, 2000);
+// setTimeout(() => {
+//     console.log("hello A-sync");
+// }, 2000);
 
-console.log("end"); 
-
-
-// ===> Q3
-function getUser(id, callback) {
-    setTimeout(() => {
-        let user = { id: id, name: "Ali" };
-        callback(user); 
-    }, 2000);
-}
-
-getUser(1, (user) => {
-    console.log(user);
-});
+// console.log("end"); 
 
 
-// ===> Q4
-function getUser(id, callback) {
-    setTimeout(() => {
-        let user = { id: id, name: "Ali" };
-        callback(user);
-    }, 2000);
-}
+// // ===> Q3
+// function getUser(id, callback) {
+//     setTimeout(() => {
+//         let user = { id: id, name: "Ali" };
+//         callback(user); 
+//     }, 2000);
+// }
 
-function getPosts(userId, callback) {
-    setTimeout(() => {
-        let posts = ["Post 1", "Post 2"];
-        callback(posts);
-    }, 2000);
-}
-
-getUser(1, (user) => {
-    console.log("User:", user);
-
-    getPosts(user.id, (posts) => {
-        console.log("Posts:", posts);
-    });
-});
+// getUser(1, (user) => {
+//     console.log(user);
+// });
 
 
+// // ===> Q4
+// function getUser(id, callback) {
+//     setTimeout(() => {
+//         let user = { id: id, name: "Ali" };
+//         callback(user);
+//     }, 2000);
+// }
 
-// ===> Q5
-function getProduct(id) {
-    return new Promise((resolve, reject) => {
-        if (id === 1) {
-            resolve({ id: 1, name: "Laptop" });
-        } else {
-            reject("Product not found");
-        }
-    });
-}
+// function getPosts(userId, callback) {
+//     setTimeout(() => {
+//         let posts = ["Post 1", "Post 2"];
+//         callback(posts);
+//     }, 2000);
+// }
 
-getProduct(1).then(product => console.log(product)).catch(error => console.error(error));
+// getUser(1, (user) => {
+//     console.log("User:", user);
+
+//     getPosts(user.id, (posts) => {
+//         console.log("Posts:", posts);
+//     });
+// });
 
 
 
+// // ===> Q5
+// function getProduct(id) {
+//     return new Promise((resolve, reject) => {
+//         if (id === 1) {
+//             resolve({ id: 1, name: "Laptop" });
+//         } else {
+//             reject("Product not found");
+//         }
+//     });
+// }
 
-// ===> Q6
-function getUser() {
-    return new Promise(resolve => {
-        setTimeout(() => {
-        resolve({ id: 1, name: "Sara" });
-        }, 2000);
-    });
-}
-
-function getPosts() {
-    return new Promise(resolve => {
-        setTimeout(() => {
-        resolve(["Post1", "Post2"]);
-        }, 3000);
-    });
-}
-
-Promise.all([getUser(), getPosts()]).then(([user, posts]) => {
-        console.log("User:", user)
-        console.log("Posts:", posts)}).catch(error => {console.log(error)});
+// getProduct(1).then(product => console.log(product)).catch(error => console.error(error));
 
 
 
-// ===> Q7
-function loadUsers() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(users => {
-            let tabs = document.getElementById('tabs');
-            users.forEach(user => {
-                let button = document.createElement('button');
-                button.innerText = user.username;
-                button.onclick = () => loadPosts(user.id);
-                tabs.appendChild(button);
-            });
-        })
-        .catch(err => console.log("Error fetching users:", err));
-}
 
-async function loadPosts(userId) {
-    let postsContainer = document.getElementById('posts');
-    postsContainer.innerHTML = "Loading posts..."; 
+// // ===> Q6
+// function getUser() {
+//     return new Promise(resolve => {
+//         setTimeout(() => {
+//         resolve({ id: 1, name: "Sara" });
+//         }, 2000);
+//     });
+// }
 
-    try {
-        let response = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
-        let posts = await response.json();
+// function getPosts() {
+//     return new Promise(resolve => {
+//         setTimeout(() => {
+//         resolve(["Post1", "Post2"]);
+//         }, 3000);
+//     });
+// }
+
+// Promise.all([getUser(), getPosts()]).then(([user, posts]) => {
+//         console.log("User:", user)
+//         console.log("Posts:", posts)}).catch(error => {console.log(error)});
+
+
+
+// // ===> Q7
+// function loadUsers() {
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//         .then(response => response.json())
+//         .then(users => {
+//             let tabs = document.getElementById('tabs');
+//             users.forEach(user => {
+//                 let button = document.createElement('button');
+//                 button.innerText = user.username;
+//                 button.onclick = () => loadPosts(user.id);
+//                 tabs.appendChild(button);
+//             });
+//         })
+//         .catch(err => console.log("Error fetching users:", err));
+// }
+
+// async function loadPosts(userId) {
+//     let postsContainer = document.getElementById('posts');
+//     postsContainer.innerHTML = "Loading posts..."; 
+
+//     try {
+//         let response = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
+//         let posts = await response.json();
         
-        postsContainer.innerHTML = `<h3>Posts for User ${userId}:</h3>`;
-        let ul = document.createElement('ul');
-        posts.forEach(post => {
-            let li = document.createElement('li');
-            li.innerText = post.title;
-            ul.appendChild(li);
-        });
-        postsContainer.appendChild(ul);
-    } catch (error) {
-        console.log("Error fetching posts:", error);
-    }
-}
+//         postsContainer.innerHTML = `<h3>Posts for User ${userId}:</h3>`;
+//         let ul = document.createElement('ul');
+//         posts.forEach(post => {
+//             let li = document.createElement('li');
+//             li.innerText = post.title;
+//             ul.appendChild(li);
+//         });
+//         postsContainer.appendChild(ul);
+//     } catch (error) {
+//         console.log("Error fetching posts:", error);
+//     }
+// }
 
-loadUsers();
+// loadUsers();
 
 
 
